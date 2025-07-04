35790a17a95aeae1c34fe833b2524f57713f1104
Amit Kumar <115470270+amitsdlv1234@users.noreply.github.com>
2025-07-04 23:04:01 +0530

    Update edit.js

--- edit.js
+++ edit.js
@@ -1,5 +1,5 @@
 
-const apiUrl="https://amitsdlv1234.github.io/user-form/api/users"
+const apiUrl="hhttps://spring-boot-api-vx4c.onrender.com/api/users"
 const params=new URLSearchParams(window.location.search);
 const userId=params.get("id");
 const form=document.getElementById("editForm");

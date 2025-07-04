9c81276f6f4b90896b5379b677bb34bbdc9d7f3b
Amit Kumar <115470270+amitsdlv1234@users.noreply.github.com>
2025-07-04 22:47:52 +0530

    first commit

---
+++ edit.js
@@ -0,0 +1,53 @@
+
+const apiUrl="https://amitsdlv1234.github.io/user-form/api/users"
+const params=new URLSearchParams(window.location.search);
+const userId=params.get("id");
+const form=document.getElementById("editForm");
+
+const nameFld=document.getElementById("name");
+const emailFld=document.getElementById("email");
+const phoneFld=document.getElementById("phone");
+const dobFld=document.getElementById("dob");
+const roleFld=document.getElementById("role");
+const idFld=document.getElementById("userId")
+
+async function loadUsers(){
+    try {
+        const res=await fetch(`${apiUrl}/${userId}`);
+        const user=await res.json();
+        idFld.value=user.id;
+        nameFld.value=user.name;
+        emailFld.value=user.email;
+        phoneFld.value=user.phone;
+        dobFld.value=user.dob;
+        roleFld.value=user.role;
+    } catch (error) {
+        alert("Failed to load user")
+    }
+}
+form.addEventListener("submit",async(e)=>{
+    e.preventDefault();
+    const updatedUser={
+        name:nameFld.value,
+        email:emailFld.value,
+        phone:phoneFld.value,
+        dob:dobFld.value,
+        role:roleFld.value
+    };
+
+    try {
+        const res=await fetch(`${apiUrl}/${userId}`,{method:"PUT",
+            headers:{"Content-Type":"application/json"},body:JSON.stringify(updatedUser)});
+            if(res.ok){
+                alert("user updated successfully!");
+                window.location.href="index.html";
+            }
+            else{
+                throw new Error(await res.text());
+            }
+    } catch (error) {
+        alert("Update failed : "+error.message);
+    }
+});
+
+window.onload=loadUsers;
---
+++ script.js
@@ -0,0 +1,95 @@
+const apiUrl="https://amitsdlv1234.github.io/user-form/api/users";
+const tbody=document.querySelector("#userTable tbody");
+const form = document.getElementById('userForm');
+
+const nameFld=document.getElementById("name");
+const emailFld=document.getElementById("email");
+const phoneFld=document.getElementById("phone");
+const dobFld=document.getElementById("dob");
+const roleFld=document.getElementById("role");
+
+
+function renderRow(user){
+    const row=document.createElement("tr");
+    row.innerHTML=`
+    <td>${user.name}</td>
+    <td>${user.email}</td>
+    <td>${user.phone}</td>
+    <td>${user.dob}</td>
+    <td>${user.role}</td>
+
+    <td>
+    <button class="edit-btn" onClick="editUser(${user.id})">Edit</button>
+    <button class="delete-btn" onClick="deleteUser(${user.id})">Delete</button>
+    </td>
+    `;
+    tbody.appendChild(row);
+}
+
+async function loadUsers(){
+    try {
+        const res=await fetch(apiUrl);
+        const users=await res.json();
+        tbody.innerHTML="";
+        users.forEach(renderRow);
+    } catch (error) {
+        alert("Failed to load users.")
+        console.log(error);
+    }
+}
+async function addUser(user){
+    try {
+        const res=await fetch(apiUrl,{method:"POST",headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(user)
+        });
+
+        if(!res.ok) throw new Error("failed to add user");
+        const newUser=await res.json();
+        alert("User added");
+        renderRow(newUser);
+        form.reset();
+    } catch (error) {
+        alert("Server Error "+error.message);
+        console.log(error);
+    }
+}
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+
+    if(!/^\d{10}$/.test(phoneFld.value.trim())){
+      alert("Phone must be exactly 10 digits.");
+      return;
+    }
+
+    const dobValue=new Date(dobFld.value);
+    const now=new Date();
+    if(dobValue>now){
+        alert("DOB can't be in the future.");
+        return;
+    }
+
+    const user={
+        name:nameFld.value.trim(),
+        email:emailFld.value.trim(),
+        phone:phoneFld.value.trim(),
+        dob:dobFld.value,
+        role:roleFld.value
+    };
+    addUser(user);
+});
+
+async function deleteUser(id){
+    if(!confirm("Delete this user?")) return;
+    try {
+        const res=await fetch(`${apiUrl}/${id}`,{method:"DELETE"});
+        if(!res.ok) throw new Error("Deletion failed");
+        loadUsers();
+    } catch (error) {
+        alert("Server error while deleting user",error.message);
+        console.log(error);
+    }
+}
+function editUser(id){
+    window.location.href=`edit.html?id=${id}`;
+}
+window.onload=loadUsers;
